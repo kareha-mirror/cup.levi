77e99d8dad9c9023bd59aa73656916d61b4e003f
Aki Kareha <aki@kareha.org>
2026-06-11 21:09:05 +0900

    Add J command

--- internal/editor/edit.go
+++ internal/editor/edit.go
@@ -1,5 +1,9 @@
 package editor
 
+import (
+	"unicode/utf8"
+)
+
 //////////////////////
 // Editing Commands //
 //////////////////////
@@ -10,10 +14,58 @@ func (ed *Editor) EditReplace(letter rune, n int) {
 	ed.Unimplemented("EditReplace")
 }
 
+func trimLeftBlanks(s string) string {
+	for i, r := range s {
+		if !isBlank(r) {
+			return s[i:]
+		}
+	}
+	return ""
+}
+
 // J : Join current line with next line.
 func (ed *Editor) EditJoin(n int) {
+	if n < 1 {
+		ed.Error("EditJoin: n < 1")
+		return
+	}
 	ed.EnsureCommand()
-	ed.Unimplemented("EditJoin")
+	if ed.row+1 >= len(ed.lines) {
+		ed.Ring("No following lines to join")
+		return
+	}
+	if n > 1 {
+		n--
+	}
+
+	current := ed.lines[ed.row]
+	col := ed.col
+
+	for i := 1; i <= n; i++ {
+		if ed.row+i >= len(ed.lines) {
+			break
+		}
+		next := trimLeftBlanks(ed.lines[ed.row+i])
+		link := ""
+		if len(next) > 0 {
+			r, _ := utf8.DecodeLastRuneInString(current)
+			if r != utf8.RuneError && !isBlank(r) {
+				link = " "
+			}
+		}
+		col = utf8.RuneCountInString(current)
+		current = current + link + next
+	}
+
+	lines := append([]string{}, ed.lines[:ed.row]...)
+	lines = append(lines, current)
+	if ed.row+1+n < len(ed.lines) {
+		lines = append(lines, ed.lines[ed.row+1+n:]...)
+	}
+	ed.lines = lines
+
+	ed.col = col
+	ed.confineCol()
 }
 
 // >> : Indent current line.
