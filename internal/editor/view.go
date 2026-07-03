3b7bddd1ffb8086517364bd4e4867aa8a670f2bf
Aki Kareha <aki@kareha.org>
2026-07-04 00:33:11 +0900

    Add view commands

--- internal/editor/view.go
+++ internal/editor/view.go
@@ -65,12 +65,53 @@ func (ed *Editor) ViewUp(n int) {
 
 // Ctrl-D : Scroll down by half view height.
 func (ed *Editor) ViewDownHalf(n int) {
-	ed.Unimplemented("ViewDownHalf")
+	i := len(ed.viewMeta) / 2
+	if i < 0 {
+		return
+	}
+	b := ed.Buf()
+	b.Loc = ed.viewMeta[i].Loc
+	b.ViewLoc = b.Loc
+	if b.Loc.Col < 1 {
+		b.Loc.Col = b.NonBlankColOfLine(b.Loc.Row)
+	}
 }
 
 // Ctrl-U : Scroll up by half view height.
 func (ed *Editor) ViewUpHalf(n int) {
-	ed.Unimplemented("ViewUpHalf")
+	b := ed.Buf()
+	viewRow := b.ViewLoc.Row - (ed.h-1)/2
+	if viewRow < 0 {
+		viewRow = 0
+	}
+	viewMeta := ed.RenderMeta(buf.Loc{0, viewRow})
+	if len(viewMeta) < 1 {
+		return
+	}
+	lastRow := viewMeta[len(viewMeta)-1].Loc.Row
+
+	if len(ed.viewMeta) < 1 {
+		return
+	}
+	topRow := ed.viewMeta[0].Loc.Row
+
+	deltaRow := topRow - lastRow - 1
+	if deltaRow < 0 {
+		deltaRow = 0
+	}
+	if deltaRow >= len(viewMeta) {
+		return
+	}
+	newViewLoc := viewMeta[deltaRow].Loc
+	viewMeta = ed.RenderMeta(newViewLoc)
+	if len(viewMeta) < 2 {
+		return
+	}
+	b.ViewLoc = newViewLoc
+	b.Loc = viewMeta[len(viewMeta)-1].Loc
+	if b.Loc.Col < 1 {
+		b.Loc.Col = b.NonBlankColOfLine(b.Loc.Row)
+	}
 }
 
 // Ctrl-Y : Scroll down by line.
@@ -121,15 +162,24 @@ func (ed *Editor) ViewUpLine(n int) {
 
 // z Enter : Reposition cursor line to top of view.
 func (ed *Editor) ViewToTop() {
-	ed.Unimplemented("ViewToTop")
+	b := ed.Buf()
+	b.Loc.Col = b.NonBlankColOfLine(b.Loc.Row)
+	b.ViewLoc.Row = b.Loc.Row
+	b.ViewLoc.Col = 0
 }
 
 // z. : Reposition cursor line middle of view.
 func (ed *Editor) ViewToMiddle() {
-	ed.Unimplemented("ViewToMiddle")
+	b := ed.Buf()
+	b.Loc.Col = b.NonBlankColOfLine(b.Loc.Row)
+	b.ViewLoc.Row = max(b.Loc.Row-(ed.h-1)/2, 0)
+	b.ViewLoc.Col = 0
 }
 
 // z- : Reposition cursor line bottom of view.
 func (ed *Editor) ViewToBottom() {
-	ed.Unimplemented("ViewToBottom")
+	b := ed.Buf()
+	b.Loc.Col = b.NonBlankColOfLine(b.Loc.Row)
+	b.ViewLoc.Row = max(b.Loc.Row-(ed.h-1)+1, 0)
+	b.ViewLoc.Col = 0
 }
